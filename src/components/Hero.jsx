14529cb6bd993af9ec8f2e9f9bba1193ac366994
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-30 19:57:31 +0530

    hero left side

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
+import InputCol from "./InputCol";
+
 const Hero = () => {
   return (
-    <div className="h-[500px] bg-[#173535]">
-      <div>
-        <p className="">Plan your love journey with us right by your side</p>
+    <div className="h-[500px] bg-[#173535] grid grid-cols-2">
+      <div className="text-white flex flex-col lg:ml-20  justify-center ">
+        <p className="xl:text-6xl lg:text-5xl md:text-3xl text-4xl font-serif ">
+          Plan your love journey with us right by your side
+        </p>
+        <div className="flex py-6 w-full">
+          <InputCol text="Category" placeholder="Venues" />
+          <InputCol text="City" placeholder="Gujarat" />
+        </div>
+        <button
+          type="button"
+          className="focus:outline-none text-[#173535] bg-white font-medium rounded-sm text-md px-6 py-2 ml-auto mr-9 "
+        >
+          View results
+        </button>
       </div>
     </div>
   );
--- src/components/InputCol.jsx
+++ src/components/InputCol.jsx
@@ -1,4 +1,15 @@
-const InputCol = () => {
-  return <div>InputCol</div>;
+const InputCol = ({ text, placeholder }) => {
+  return (
+    <div className="w-full relative p-6">
+      <h2 className="bg-[#173535] absolute z-10 text-sm top-[10px] p-1 left-[32px] tracking-wide">
+        {text}
+      </h2>
+      <input
+        className="w-full bg-[#173535] py-2 px-3 rounded-md border border-white placeholder:text-white"
+        type="text"
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 export default InputCol;
