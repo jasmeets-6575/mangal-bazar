7e11faa226cfabee1c25185ba94d90cb32bd2ce0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-30 20:04:13 +0530

    category list

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,22 @@
+import CategoriesList from "./CategoriesList";
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div className="  flex flex-col items-center justify-center px-10 pt-10 bg-[#F5F5F5] ">
+      <div className="md:text-6xl text-4xl font-bold font-sans ">
+        <h2>Wedding Categories</h2>
+      </div>
+      <div className="w-3/5 text-center mt-10">
+        <p className="md:text-lg font-serif tracking-wide">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+          temporibus repellat reiciendis magni nemo ipsum voluptatibus laborum
+          nulla doloribus
+        </p>
+      </div>
+      <div className="mt-10">
+        <CategoriesList />
+      </div>
+    </div>
+  );
 };
 export default Categories;
