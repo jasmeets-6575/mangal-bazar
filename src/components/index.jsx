6060f24baa274dce59e337d0bbcfe32e95451cd2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-30 21:41:49 +0530

    gallery

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,7 +2,6 @@ import Categories from "./Categories";
 import Footer from "./Footer";
 import Gallery from "./Gallery";
 import Hero from "./Hero";
-import Links from "./Links";
 import Navbar from "./Navbar";
 
-export { Categories, Footer, Gallery, Hero, Links, Navbar };
+export { Categories, Footer, Gallery, Hero, Navbar };
